67cfd23344b0d7c87532ae2e2356185e199af100
akarellano2 <55068546+akarellano2@users.noreply.github.com>
2019-09-26 07:14:11 -0500

    Update HW5.py

--- HW5.py
+++ HW5.py
@@ -35,6 +35,65 @@ class SinglyLinkedList:
             other = other.next
             tmp.next = temp
             temp.next = other
+            
+    def clear(self):
+        self.head = None
+
+    def contains_item(self, item):
+        tmp = self.head
+        while tmp is not None:
+            if item is tmp.item:
+                return True
+                break
+
+            else:
+                tmp = tmp.next
+        return False
+
+
+    def get_index_of(self, item):
+        temp = self.head
+        while temp is not None:
+            if item is temp.item:
+                return temp
+                break
+
+            else:
+                temp = temp.next
+
+    def get(self, index):
+        count = 0
+        tmp = self.head
+
+        if tmp is None:
+            return None
+
+        else:
+            while count is not index:
+                if tmp.next is None:
+                    return None
+                count += 1
+                tmp = tmp.next
+            return tmp.item
+
+    def get_first(self):
+        if self is None:
+            return -1
+        else:
+            return self.head
+
+    def get_last(self):
+        temp = self.head
+        while temp is not None:
+            if temp.next is None:
+                return temp.item
+
+            else:
+                temp = temp.next
+
+    def remove_index(self, index):
+        count = 0
+        temp = self.head
 
 
 
